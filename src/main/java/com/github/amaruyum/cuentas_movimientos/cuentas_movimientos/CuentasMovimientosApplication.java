6ee9d881710cd99aa1d71b827411be8cc93bbb49
Atik yumbay <amaruyum@gmail.com>
2025-08-01 10:42:02 -0500

    Reto Tecnico Java cuentas-movimientos

---
+++ src/main/java/com/github/amaruyum/cuentas_movimientos/cuentas_movimientos/CuentasMovimientosApplication.java
@@ -0,0 +1,13 @@
+package com.github.amaruyum.cuentas_movimientos.cuentas_movimientos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuentasMovimientosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuentasMovimientosApplication.class, args);
+	}
+
+}
